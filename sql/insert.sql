609e0154b23c83d195c7a37583dcf0a6a805718c
torentlikher <torentlikher@gmail.com>
2021-07-13 11:51:00 -0400

    initial commit

---
+++ sql/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO Snakes (species, weight_lb, length, color, price)
+VALUES ('Rattlesnake', 5, 10, 'beige', 100);
+
+INSERT INTO Customers (first_name, last_name, address, city, state, postal, country, email)
+VALUES ('Tim', 'Orentlikher', '123 Main St', 'Somewhere', 'NY', 10543, 'US', 'tim@snakesrus.com');
+
+INSERT INTO DeliveryPartners (organization_name, delivery_method)
+VALUES ('Snakes On A Plane', 'Air');
+
+INSERT INTO Breeders (breeder_name, email, phone_number)
+VALUES( 'A1 Reptiles Inc', 'foo@bar.com', '0001112222')
