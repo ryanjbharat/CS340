11f76991ac366ba552741ebb4412c381ed801cc8
Ryan Bharat <bharatr@flip3.engr.oregonstate.edu>
2021-07-19 15:04:51 -0700

    Please enter the commit message for your changes. Lines starting

---
+++ app.py
@@ -0,0 +1,157 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQLdb
+from config import user, password, database
+## create config file with your user, password, db to replicate
+## gunicorn --bind 0.0.0.0:5642 wsgi:app -D
+
+host ='classmysql.engr.oregonstate.edu'
+
+
+app = Flask(__name__)
+db = MySQLdb.connect(host, user, password, database)
+
+
+@app.route('/')
+def root():
+    #citation: https://www.w3schools.in/python-tutorial/database-connection/
+    cursor = db.cursor()
+    cursor.execute('''SELECT 'Maybe some aggregate stats can go here...';''')
+    results = cursor.fetchall()
+    cursor.close()
+
+    return render_template('index.html', results = str(results))
+
+@app.route('/Orders')
+def Orders():
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM OrdersHeaders;')
+    results = cursor.fetchall()
+    cursor.close()
+
+    return render_template('Orders.html', results = results)
+
+
+@app.route('/Orders/Detail/<int:orderid>')
+def Detail(orderid):
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM OrdersHeaders o JOIN OrdersDetails d WHERE o.order_id = d.orderid AND o.order_id = ' + str(orderid))
+    results = cursor.fetchall()
+    cursor.close()
+
+    return render_template('OrderDetail.html', results = results)
+
+
+@app.route('/Orders/NewOrder', methods=['GET', 'POST'])
+def CreateOrder():
+    cursor = db.cursor()
+    if request.method == 'POST':
+        #get user input from form
+        #customer_id = request.form['customer']
+        #partner_id = request.form['partner']
+        #cursor.execute('INSERT INTO DeliveryPartners (organization_name,delivery_method) VALUES (%s, %s);', (org_name,delivery_method))
+        #commit() is needed to save the changes... otherwise the insert statement is not saved.
+        #citation: https://www.w3schools.com/python/python_mysql_insert.asp
+        db.commit()
+        cursor.close()
+
+        return redirect(url_for('Orders'))
+    else:
+        cursor = db.cursor()
+        cursor.execute('SELECT customer_id, email FROM Customers')
+        customers = cursor.fetchall()
+        cursor.execute('SELECT delivery_partner_id, organization_name FROM DeliveryPartners')
+        partners = cursor.fetchall()
+        cursor.close()
+        
+        return render_template('NewOrder.html', customers=customers, partners=partners)
+
+
+@app.route('/Snakes')
+def Snakes():
+    cursor = db.cursor()
+    cursor.execute('''SELECT 'Snake Query here...';''')
+    results = cursor.fetchall()
+    cursor.close()
+
+    return render_template('Snakes.html', results=results)
+
+
+@app.route('/Breeders')
+def Breeders():
+    cursor = db.cursor()
+    cursor.execute('''SELECT 'foo bar';''')
+    results = cursor.fetchall()
+    cursor.close()
+    
+    return render_template('Breeders.html', results=results)
+
+
+@app.route('/Customers')
+def Customers():
+    cursor = db.cursor()
+    cursor.execute('''SELECT 'Customers query here...';''')
+    results = cursor.fetchall()
+    cursor.close()
+    
+    return render_template('Customers.html', results=results)
+
+
+@app.route('/DeliveryPartners')
+def DeliveryPartners():
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM DeliveryPartners;')
+    results = cursor.fetchall()
+    cursor.close()
+    
+    return render_template('DeliveryPartners.html', results=results)
+
+
+@app.route('/DeliveryPartners/DeletePartner/<int:partnerid>')
+def DeleteDeliveryPartner(partnerid):
+    cursor = db.cursor()
+    cursor.execute('DELETE FROM DeliveryPartners WHERE delivery_partner_id=' + str(partnerid))
+    db.commit()
+    cursor.close()
+
+    return redirect(url_for('DeliveryPartners'))
+
+
+@app.route('/DeliveryPartners/NewPartner', methods=['GET', 'POST'])
+def CreateDeliveryPartner():
+    if request.method == 'POST':
+        cursor = db.cursor()
+        #get user input from form
+        org_name = request.form['org']
+        delivery_method = request.form['method']
+        cursor.execute('INSERT INTO DeliveryPartners (organization_name,delivery_method) VALUES (%s, %s);', (org_name,delivery_method))
+        #commit() is needed to save the changes... otherwise the insert statement is not saved.
+        #citation: https://www.w3schools.com/python/python_mysql_insert.asp
+        db.commit()
+        cursor.close()
+
+        return redirect(url_for('DeliveryPartners'))
+    else:
+        return render_template('NewPartner.html')
+
+@app.route('/DeliveryPartners/EditPartner/<int:partnerid>', methods=['GET', 'POST'])
+def EditDeliveryPartner(partnerid):
+    cursor = db.cursor()
+    if request.method == 'POST':
+        #get user input from form
+        org_name = request.form['org']
+        delivery_method = request.form['method']
+        cursor.execute('UPDATE DeliveryPartners SET organization_name = %s, delivery_method = %s WHERE delivery_partner_id = %s;', (org_name, delivery_method, partnerid))
+        db.commit()
+        cursor.close()
+
+        return redirect(url_for('DeliveryPartners'))
+    else:
+        cursor.execute('SELECT * FROM DeliveryPartners WHERE delivery_partner_id = ' + str(partnerid))
+        results = cursor.fetchone()
+        cursor.close()
+        return render_template('EditPartner.html', results=results)
+
+
+if __name__ == "__main__": 
+    #Change to debug=False for final submission  
+    app.run(debug=True) 
---
+++ sql/schema.sql
@@ -0,0 +1,58 @@
+CREATE TABLE Snakes(
+    snake_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    species VARCHAR(50) NOT NULL,
+    weight_lb INT NOT NULL,
+    length DOUBLE(6,2) NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    price DOUBLE(6,2) NOT NULL
+);
+    
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	address VARCHAR(50) NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	state VARCHAR(2) NOT NULL,
+	postal VARCHAR(10) NOT NULL, 
+	country VARCHAR(50) NOT NULL, 
+	email VARCHAR(50) UNIQUE
+);
+  
+CREATE TABLE DeliveryPartners(
+    delivery_partner_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	organization_name VARCHAR(50) NOT NULL,
+	delivery_method VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Breeders(
+    breeder_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    breeder_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    phone_number VARCHAR(10)
+);
+
+CREATE TABLE SnakesBreeders(
+    snake_id INT NOT NULL,
+    breeder_id INT NOT NULL,
+    PRIMARY KEY (snake_id, breeder_id),
+    FOREIGN KEY (snake_id) REFERENCES Snakes(snake_id),
+    FOREIGN KEY (breeder_id) REFERENCES Breeders(breeder_id)
+);
+
+CREATE TABLE OrdersHeaders(
+	order_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    customer_id INT NOT NULL,
+    delivery_partner_id INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (delivery_partner_id) REFERENCES DeliveryPartners(delivery_partner_id)
+);
+
+CREATE TABLE OrdersDetails(
+    order_detail_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    order_id INT NOT NULL,
+    snake_id INT NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES OrdersHeaders(order_id),
+    FOREIGN KEY (snake_id) REFERENCES Snakes(snake_id)
+);
